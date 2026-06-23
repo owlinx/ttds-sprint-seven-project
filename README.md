20b6712c40abb83ce1359857f95f10ed7d7eafc4
owlinx <eoegl@proton.me>
2026-06-23 11:26:36 -0600

    inicio, botones de histograma y graf. de dispercion

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ttds-sprint-seven-project
+
+Esta aplicacion muestra un histograma o un grafico de disperción segun el boton que sea precionado
---
+++ app.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
+import streamlit as st
+
+# Leer los datos del archivo CSV
+car_data = pd.read_csv('vehicles_us.csv')
+
+print(car_data['odometer'])
+
+# Crear un botón en la aplicación Streamlit
+hist_button = st.button('Construir histograma')
+
+# Lógica a ejecutar cuando se hace clic en el botón
+if hist_button:
+    # Escribir un mensaje en la aplicación
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir un gráfico de disperción')
+
+if scatter_button:
+    st.write('Creación de un gráfico de disperción para el conjunto de datos de anuncios de venta de coches')
+
+    fig = go.Figure(
+        data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+    st.plotly_chart(fig, use_container_width=True)
